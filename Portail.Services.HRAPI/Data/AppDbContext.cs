138dc53db4db335ed8049603abb2ce5d4db787d0
Zayd EL IMRANI <elimrani.z@gmail.com>
2023-08-10 08:04:02 +0100

    Add project files.

---
+++ Portail.Services.HRAPI/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portal.Services.HRAPI.Model;
+
+namespace Portal.Services.HRAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<HumanResource> HumanResource { get; set; }
+
+    }
+}
