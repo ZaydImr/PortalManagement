55084ab4d4382f1852f95931ac241ed5f1949560
Zayd EL IMRANI <elimrani.z@gmail.com>
2023-08-26 21:44:54 +0100

    Updating models and DataContext

--- Portail.Services.DataAPI/Model/ViewModel/DepartmentVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Portal.Services.DataAPI.Model.ViewModel;
-
-public class DepartmentVM
-{
-    public int DepartmentID { get; set; }
-    public required string Name { get; set; }
-    //public string? Description { get; set; }
-}
